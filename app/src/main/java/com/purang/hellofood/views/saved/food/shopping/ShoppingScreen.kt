2215ca7b164eda3a90c0095095674c08bc038f8b
sonms <smsms5676@naver.com>
2025-04-18 21:00:51 +0900

    feat/저장된 shopping, recipes 데이터 보여주는 리스트 화면

---
+++ app/src/main/java/com/purang/hellofood/views/saved/food/shopping/ShoppingScreen.kt
@@ -0,0 +1,62 @@
+package com.purang.hellofood.views.saved.food.shopping
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavController
+import com.purang.hellofood.utils.FirebaseUserManager
+import com.purang.hellofood.viewmodels.FoodLogViewModel
+import com.purang.hellofood.viewmodels.GeminiViewModel
+import com.purang.hellofood.views.saved.food.FoodItemUI
+
+@Composable
+fun ShoppingScreen(
+    navController: NavController,
+    foodLogViewModel: FoodLogViewModel = hiltViewModel()
+) {
+    val shoppingList by foodLogViewModel.shoppingList.collectAsState(emptyList())
+    val userId = FirebaseUserManager.userId.toString()
+
+    LaunchedEffect(Unit) {
+        foodLogViewModel.fetchShoppingList(userId)
+    }
+
+    Column (
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+    ) {
+        Text(
+            text = "Shopping List",
+            modifier = Modifier.padding(bottom = 8.dp),
+            fontWeight = FontWeight.Bold
+        )
+
+        LazyColumn {
+            itemsIndexed(
+                items = shoppingList
+            ) { _ , item ->
+                FoodItemUI(
+                    item = item,
+                    onClickItem = {
+
+                    },
+                    onLongClickItem = {
+
+                    }
+                )
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/purang/hellofood/views/saved/recipe/list/RecipeListScreen.kt
@@ -0,0 +1,63 @@
+package com.purang.hellofood.views.saved.recipe.list
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavController
+import com.purang.hellofood.utils.FirebaseUserManager
+import com.purang.hellofood.viewmodels.FoodLogViewModel
+import com.purang.hellofood.views.saved.food.FoodItemUI
+
+@Composable
+fun RecipeListScreen(
+    navController: NavController,
+    foodLogViewModel: FoodLogViewModel = hiltViewModel()
+) {
+    val recipeList by foodLogViewModel.recipeList.collectAsState(emptyList())
+    val userId = FirebaseUserManager.userId.toString()
+
+    LaunchedEffect(Unit) {
+        foodLogViewModel.fetchRecipeList(userId)
+    }
+
+    Column (
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+    ) {
+        Text(
+            text = "Shopping List",
+            modifier = Modifier.padding(bottom = 8.dp),
+            fontWeight = FontWeight.Bold
+        )
+
+        LazyColumn {
+            itemsIndexed(
+                items = recipeList
+            ) { _ , item ->
+                FoodItemUI(
+                    item = item,
+                    onClickItem = {
+                        //음식이름으로 영양소 도출? todo
+                        
+                    },
+                    onLongClickItem = {
+
+                    }
+                )
+            }
+        }
+    }
+
+}
